cfd29bba73cb60f15debcf812d687b511a7a3618
whitenight369 <92344487+whitenight369@users.noreply.github.com>
2023-09-11 07:45:31 +0800

    feat: 添加远程库

---
+++ demo.js
@@ -0,0 +1,16 @@
+const a = "133";
+const b = "asdasd";
+
+const test = (v) => {
+  switch (v) {
+    case 1:
+      const obj = {};
+    case 2:
+      const ob = {};
+    default:
+      "";
+  }
+  return "";
+};
+
+console.log(test(1));
